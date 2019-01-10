6c2560f64657fa15c3f795b867773bdd516de2be
micromic5 <mike.amacker@gmx.ch>
2019-01-10 10:31:03 +0100

    Revert "added gitignore"
    This reverts commit a58e9f2b3b0c021196146faab221105ab9e7f6ba.

---
+++ satisFeetApp/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-geolocation.geolocation",
+    "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "clobbers": [
+      "navigator.geolocation"
+    ]
+  },
+  {
+    "id": "cordova-plugin-geolocation.PositionError",
+    "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+    "pluginId": "cordova-plugin-geolocation",
+    "runs": true
+  },
+  {
+    "id": "cordova-pedometer.Pedometer",
+    "file": "plugins/cordova-pedometer/www/pedometer.js",
+    "pluginId": "cordova-pedometer",
+    "clobbers": [
+      "pedometer"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-geolocation": "4.0.1",
+  "cordova-pedometer": "0.2.0"
+};
+// BOTTOM OF METADATA
+});
