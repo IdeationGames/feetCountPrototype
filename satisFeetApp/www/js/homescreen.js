b4151ab003245ea5fd6d81629702fc4afb787925
TheCell <simon_hischier@hotmail.com>
2019-01-10 18:38:14 +0100

    fixed slider height

--- satisFeetApp/www/js/homescreen.js
+++ satisFeetApp/www/js/homescreen.js
@@ -3,9 +3,16 @@ let instance;
 document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.carousel');
    
-    instance = M.Carousel.init(elems, {
+    window.instance = M.Carousel.init(
+        elems, {
         fullWidth: true,
+        duration: 10,
         indicators: true
+    });
+    
+    elems.forEach(function (ele, key, arr)
+    {
+        ele.style.height = "200px";
     });
 });
 
